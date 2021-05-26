c253af814c044ae4ba615ad055b8d82a27c2f1d4
unknown <hegedus.arpad@gmail.com>
2021-05-26 10:40:42 +0100

    utilities and animations mostly done

--- map-folders.js
+++ map-folders.js
@@ -1,6 +1,18 @@
 import {readdir, writeFile} from 'fs/promises'
 
-["component", "theme", "utility"].forEach(mod => {
+// ['animation'].forEach(mod => {
+//     readdir(`./src/${mod}`).then(folders => {
+//         let fileList = '';
+//         folders.forEach(folder => {
+//             readdir(`./src/${mod}/${folder}`).then(files => {
+//                 fileList = `${fileList}${(fileList === '' ? '' : '\n')}@forward "${mod}/${folder}/${files.map(file => file.replace('.scss', '')).join(`";\n@forward "${mod}/${folder}/`)}";`
+//                 writeFile(`./src/${mod}.scss`, fileList.split('\n').sort().join('\n'))
+//             })
+//         })
+//     })
+// })
+
+['theme', 'utility'].forEach(mod => {
     readdir(`./src/${mod}`).then(files => {
         files = `@forward "${mod}/${files.map(file => file.replace('.scss', '')).join(`";\n@forward "${mod}/`)}";`
         writeFile(`./src/${mod}.scss`, files)
